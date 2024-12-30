8d24a2ff3e419099dbefea6870dcfcf4d5b2f1e9
Gianluca Pagliara <pagliara.gianluca@gmail.com>
2024-12-30 19:59:39 +0100

    feat: initial implementation of agentspype package

---
+++ agentspype/agent/publishing.py
@@ -0,0 +1,54 @@
+from enum import Enum
+from typing import TYPE_CHECKING, Any
+
+from eventspype.publishers.multi import MultiPublisher
+from eventspype.publishers.publications import EventPublication
+from statemachine import State
+
+if TYPE_CHECKING:
+    from .agent import Agent
+
+
+class AgentPublishing(MultiPublisher):
+    def __init__(self, agent: "Agent") -> None:
+        super().__init__()
+        self._agent = agent
+
+    # === Properties ===
+
+    @property
+    def agent(self) -> "Agent":
+        return self._agent
+
+
+class StateAgentPublishing(AgentPublishing):
+    # === Definitions ===
+
+    class Events(Enum):
+        StateMachineTransition = "sm_transition_event"
+
+    class StateMachineEvent:
+        event: Any
+        new_state: State
+
+    # === Publications ===
+
+    sm_transition_event = EventPublication(
+        event_tag=Events.StateMachineTransition, event_class=StateMachineEvent
+    )
+
+    # === Events ===
+
+    def publish_transition(self, event: Any, state: State) -> None:
+        """
+        Publishes a state machine transition event.
+
+        It is recommended to call this method from the state machine.
+        Example:
+            def after_transition(self, event, state):
+                self.agent.publishing.publish_transition(event, state)
+        """
+        self.trigger_event(
+            self.sm_transition_event,
+            self.sm_transition_event.event_class(event, state),
+        )
